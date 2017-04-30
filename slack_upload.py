79fca2d477560d28a4bc852734a9eec01e26f4e3
mhvdrone <mhvdrone@gmail.com>
2017-04-30 06:58:52 -0500

    more configurable

--- slack_upload.py
+++ slack_upload.py
@@ -3,7 +3,9 @@ import io
 import os
 
 slack_client = SlackClient(os.environ.get('SLACK_API_TOKEN'))
-r = slack_client.api_call("files.upload", filename='3D Printers.jpg', channels='#webcams',
+r = slack_client.api_call("files.upload",
+    filename=os.environ.get('UPLOAD_NAME'),
+    channels=os.environ.get('SLACK_CHANNEL'),
     file=io.BytesIO(open('image.jpg','r').read())
     )
 image_id = r['file']['id']
